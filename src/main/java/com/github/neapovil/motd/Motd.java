aa8911b4a90791bb1c3f04b2e5d5afa37015bf2e
William <nearata@protonmail.com>
2024-02-20 12:58:53 +0100

    1.19.4

--- src/main/java/com/github/neapovil/motd/Motd.java
+++ src/main/java/com/github/neapovil/motd/Motd.java
@@ -1,6 +1,7 @@
 package com.github.neapovil.motd;
 
-import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.List;
 
 import org.apache.commons.lang.StringUtils;
@@ -9,8 +10,10 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.server.ServerListPingEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import com.electronwill.nightconfig.core.file.FileConfig;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
+import dev.jorel.commandapi.CommandAPI;
 import dev.jorel.commandapi.CommandAPICommand;
 import dev.jorel.commandapi.arguments.BooleanArgument;
 import dev.jorel.commandapi.arguments.GreedyStringArgument;
@@ -24,8 +27,9 @@ import net.kyori.adventure.text.minimessage.MiniMessage;
 public final class Motd extends JavaPlugin implements Listener
 {
     private static Motd instance;
-    private FileConfig config;
     private final MiniMessage miniMessage = MiniMessage.miniMessage();
+    private Config config;
+    private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
     @Override
     public void onEnable()
@@ -34,30 +38,39 @@ public final class Motd extends JavaPlugin implements Listener
 
         this.saveResource("config.json", false);
 
-        this.config = FileConfig.builder(new File(this.getDataFolder(), "config.json"))
-                .autoreload()
-                .autosave()
-                .build();
-        this.config.load();
+        this.load();
 
         this.getServer().getPluginManager().registerEvents(this, this);
 
         new CommandAPICommand("motd")
                 .withPermission("motd.command")
                 .withArguments(new LiteralArgument("set"))
-                .withArguments(new MultiLiteralArgument("string1", "string2"))
-                .withArguments(new LiteralArgument("text"))
+                .withArguments(new MultiLiteralArgument("setting", "string1", "string2"))
                 .withArguments(new GreedyStringArgument("newtext"))
                 .executes((sender, args) -> {
-                    final String setting = (String) args[0];
-                    final String newtext = (String) args[1];
+                    final String setting = (String) args.get("setting");
+                    final String newtext = (String) args.get("newtext");
 
-                    this.config.set("config." + setting, newtext);
+                    if (setting.equals("string1"))
+                    {
+                        this.config.string1 = newtext;
+                    }
 
-                    final Component component = Component.text("Motd " + setting + " changed to:\n")
-                            .append(this.miniMessage.deserialize(newtext));
+                    if (setting.equals("string2"))
+                    {
+                        this.config.string2 = newtext;
+                    }
 
-                    sender.sendMessage(component);
+                    try
+                    {
+                        this.save();
+                        sender.sendRichMessage("Motd " + setting + " text changed to: " + newtext);
+                    }
+                    catch (IOException e)
+                    {
+                        CommandAPI.failWithString("Unable to save.");
+                        this.getLogger().severe(e.getMessage());
+                    }
                 })
                 .register();
 
@@ -67,11 +80,18 @@ public final class Motd extends JavaPlugin implements Listener
                 .withArguments(new LiteralArgument("centered"))
                 .withArguments(new BooleanArgument("bool"))
                 .executes((sender, args) -> {
-                    final boolean bool = (boolean) args[0];
+                    final boolean bool = (boolean) args.get("bool");
 
-                    this.config.set("config.centered", bool);
-
-                    sender.sendMessage("Motd centered status changed to: " + bool);
+                    try
+                    {
+                        this.save();
+                        sender.sendMessage("Motd centered status changed to: " + bool);
+                    }
+                    catch (IOException e)
+                    {
+                        CommandAPI.failWithString("Unable to save.");
+                        this.getLogger().severe(e.getMessage());
+                    }
                 })
                 .register();
     }
@@ -81,23 +101,49 @@ public final class Motd extends JavaPlugin implements Listener
     {
     }
 
-    public static Motd getInstance()
+    public static Motd instance()
     {
         return instance;
+    }
+
+    private void load()
+    {
+        try
+        {
+            final String string = Files.readString(this.getDataFolder().toPath().resolve("config.json"));
+            this.config = this.gson.fromJson(string, Config.class);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    private void save() throws IOException
+    {
+        final String string = this.gson.toJson(this.config);
+        Files.write(this.getDataFolder().toPath().resolve("config.json"), string.getBytes());
     }
 
     @EventHandler
     private void serverListPing(ServerListPingEvent event)
     {
-        final List<Component> motd = List.of((String) this.config.get("config.string1"), (String) this.config.get("config.string2"))
+        final List<Component> motd = List.of(this.config.string1, this.config.string2)
                 .stream()
                 .map(s -> this.miniMessage.deserialize(s))
-                .map(s -> (boolean) this.config.get("config.centered") ? s.replaceText(b -> {
+                .map(s -> this.config.centered ? s.replaceText(b -> {
                     b.match(((TextComponent) s).content())
                             .replacement((b1, c) -> Component.text(StringUtils.center(c.content(), 59)));
                 }) : s)
                 .toList();
 
         event.motd(Component.join(JoinConfiguration.separator(Component.text("\n")), motd));
+    }
+
+    class Config
+    {
+        public boolean centered;
+        public String string1;
+        public String string2;
     }
 }
